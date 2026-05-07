6379b1d946492b76bedc0e617d70ff8560f5b457
Roshaan Zaib <m.roshaanzaib@gmail.com>
2026-05-08 00:50:41 +0500

    fix firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyACf_Q2M3lh3w7Fq5K2C9ku64IsHCfhArE",
+  authDomain: "expense-tracker-c7acc.firebaseapp.com",
+  projectId: "expense-tracker-c7acc",
+  storageBucket: "expense-tracker-c7acc.firebasestorage.app",
+  messagingSenderId: "365823667886",
+  appId: "1:365823667886:web:9a466eba4b381e7171c73f"
 }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
